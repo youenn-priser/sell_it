48eafc9881d2a7e3b6ed4e0c809155b764a69af2
youenn priser <youenn.priser@gmail.com>
2021-03-10 10:17:11 +0100

    Init authentication - to be continued

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   get 'ping', to: 'application#ping'
 
   resources :classifieds, only: %w(index show)
+
+  resource :users, only: [:create]
+  post "/login", to: "users#login"
+  get "/auto_login", to: "users#auto_login"
 end
